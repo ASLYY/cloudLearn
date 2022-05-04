c7f650fc9e09640f6ee59fc5e173f535932c8bc4
ASLYY <ASLYY123_JJ@163.com>
2022-05-04 16:49:00 +0800

    first commit

---
+++ cloud_provider_payment/src/main/java/com/springcloud/PaymentMain.java
@@ -0,0 +1,9 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+
+public class PaymentMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain.class, args);
+    }
+}
