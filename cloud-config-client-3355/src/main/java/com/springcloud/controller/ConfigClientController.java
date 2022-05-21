f15fd9a46ea1b4d51efce198f9fde626b5a7f77c
ASLYY <ASLYY123_JJ@163.com>
2022-05-21 16:59:40 +0800

    configServer + bus

---
+++ cloud-config-client-3355/src/main/java/com/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RefreshScope
+//需要热加载的bean需要加上@RefreshScope注解，当配置发生变更的时候可以在不重启应用的前提下完成bean中相关属性的刷新。
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+
+
+}
